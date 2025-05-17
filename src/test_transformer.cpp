7eef8b9e00d81590ecd572ebcc2f034c890897b3
Arun <amuthu165@gmail.com>
2025-05-17 11:30:57 +0530

    jsm

--- src/test_transformer.cpp
+++ src/test_transformer.cpp
@@ -1,5 +1,6 @@
 #include "detection/detector.h"
 #include <ATen/core/TensorBody.h>
+#include <opencv2/core/mat.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/videoio.hpp>
@@ -7,12 +8,12 @@
 #include <chrono>
 
 void test(cv::Mat& image){
-    detector::transformer model("deps/detr_scripted.pt");
+    detector::transformer model("/home/arun/projects/hackathon/deps/detr_scripted.pt");
     std::tuple<at::Tensor, at::Tensor> output = model.run_inference(image);
     detector::draw_boxes(output,image);
 }
 void test(cv::Mat&& image){
-    detector::transformer model("deps/detr_scripted.pt");
+    detector::transformer model("/home/arun/projects/hackathon/deps/detr_scripted.pt");
     std::tuple<at::Tensor, at::Tensor> output = model.run_inference(image);
     detector::draw_boxes(output,image);
     cv::imshow("Test Image",image);
@@ -21,18 +22,19 @@ void test(cv::Mat&& image){
 
 
 int main(){
-    cv::VideoCapture video("deps/Crowd walking on street.mp4");
+    cv::VideoCapture video("/home/arun/projects/hackathon/deps/Crowd walking on street.mp4"),output_video;
     cv::Mat frame;
-    test(cv::imread("deps/warehouse.jpeg"));
+    test(cv::imread("/home/arun/projects/hackathon/deps/warehouse.jpeg"));
+
     std::cin.get();
-    video >> frame;
-    // cv::namedWindow("Video");
-    while(!frame.empty()){
+    while(video.isOpened()){
         std::cout << "Inside Loop\n";
-        test(frame);
+        
         video >> frame;
+        test(frame);
+        
         cv::imshow("Video",frame);
-        cv::waitKey(0);
+        cv::waitKey(5);
 
     }
 }
